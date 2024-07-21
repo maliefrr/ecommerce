324ac5be9d56bf71c2bc8cd4b7b091960f99ac6b
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2024-07-21 21:40:49 +0700

    fix: fix bug on payment confirmation id

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -5,6 +5,7 @@ import { showProduct } from "../controller/Product/showProduct.js"
 import { protect } from "../utils/protect.js"
 import { detailProduct } from "../controller/Product/detailProduct.js"
 import { buyProduct } from "../controller/User/buyProduct.js"
+import { updatePaymentConfirmation } from "../controller/Admin/updatePaymentConfirmation.js"
 
 const router = Router()
 
@@ -14,6 +15,7 @@ router.get('/:id',detailProduct)
 router.post('/add-category', protect, addCategory)
 router.post('/add-product', protect, addProduct)
 router.post('/buy', protect, buyProduct)
+router.post('/update-payment-confirmation', protect, updatePaymentConfirmation)
 
 
 export default router
