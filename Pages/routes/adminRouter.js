4f77575b7d32dca77ec020c43cfb51cfa8b1cf6b
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2024-07-22 20:03:16 +0700

    feat: add product page

--- Pages/routes/adminRouter.js
+++ Pages/routes/adminRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
-import { addCategoryPage } from "../controller/Auth/addCategoryPage.js"
+import { addCategoryPage } from "../controller/Admin/addCategoryPage.js"
+import { addProductPage } from "../controller/Admin/addProductPage.js"
 
 
 const router = Router()
 
 router.get('/add-category', addCategoryPage)
+router.get('/add-product', addProductPage)
 
 
 export default router
